f866fb5a3d2457d6f0cba25dbd0b327d7adc5610
Ed Steinbauer <steinbauer@fas.harvard.edu>
2017-02-10 14:05:46 -0500

    created initial file structure

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Tip Splitter</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+  </head>
+  <body>
+    <h1>Tip Splitter </h1>
+  </body>
+</html>
